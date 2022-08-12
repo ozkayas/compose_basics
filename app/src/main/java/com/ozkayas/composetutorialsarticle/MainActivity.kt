3ee97277991eed4bb25a942479000b87a9440948
Suat Özkaya <suatozkaya@gmail.com>
2022-08-12 23:09:25 +0300

    Complete quadrants exercise

--- app/src/main/java/com/ozkayas/composetutorialsarticle/MainActivity.kt
+++ app/src/main/java/com/ozkayas/composetutorialsarticle/MainActivity.kt
@@ -4,18 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,9 +32,43 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
 //                    ArticleMainLayout()
-                    TaskMainLayout()
+//                    TaskMainLayout()
+                    QuadrantMainLayout()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun QuadrantMainLayout(){
+    Column{
+        Row(Modifier.weight(1f)) {
+            Quadrant(color = Color.Green, title = stringResource(R.string.title_1), description = stringResource(
+                R.string.description_1
+            ), flex = 1f )
+            Quadrant(color = Color.Yellow, title = stringResource(R.string.title_2), description = stringResource(
+                R.string.description_2), flex = 1f)
+        }
+        Row(Modifier.weight(1f)) {
+            Quadrant(color = Color.Cyan, title = stringResource(R.string.title_3), description = stringResource(
+                R.string.description_3), flex = 1f)
+            Quadrant(color = Color.LightGray, title = stringResource(R.string.title_4), description = stringResource(
+                R.string.description_4), flex = 1f)
+        }
+    }
+}
+
+@Composable
+fun RowScope.Quadrant(color:Color, title:String, description:String, flex: Float ){
+    Surface(color = color, modifier = Modifier
+        .fillMaxHeight()
+        .weight(flex)) {
+        Column(modifier = Modifier.padding(all = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center){
+            Text(text = title, fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 16.dp))
+            Text(text = description, textAlign = TextAlign.Justify)
         }
     }
 }
